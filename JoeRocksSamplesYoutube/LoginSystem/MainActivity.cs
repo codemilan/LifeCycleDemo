f015341b34eb629521d2988c6b9b2cbc3fbd03a7
codemilan <mrawal47@gmail.com>
2016-10-03 12:27:20 +0545

    added designer for dialog for registration form and added style xml for sign_up_dialog layout

--- JoeRocksSamplesYoutube/LoginSystem/MainActivity.cs
+++ JoeRocksSamplesYoutube/LoginSystem/MainActivity.cs
@@ -11,13 +11,25 @@ namespace LoginSystem
     [Activity(Label = "LoginSystem", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private Button mBtnSignUP;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.Main);
-            
+
+            mBtnSignUP = FindViewById<Button>(Resource.Id.btnSignUp);
+
+            mBtnSignUP.Click += (Object sender, EventArgs args) =>
+            {
+                // Pull up dialog
+                // fragment transaction are use to control adding and removing of fragments
+                // in proper order.
+                FragmentTransaction transaction = FragmentManager.BeginTransaction();
+                dialog_SignUp signUpDialog = new dialog_SignUp();
+                signUpDialog.Show(transaction, "dialog fragment");
+            };
         }
     }
 }
