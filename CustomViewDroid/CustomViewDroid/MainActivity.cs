4c150b8cd3fb0ae7f8fbde2c81ca98f72ce132b2
codemilan <mrawal47@gmail.com>
2016-10-27 18:03:51 +0545

    developed demo for custom view practice

---
+++ CustomViewDroid/CustomViewDroid/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace CustomViewDroid
+{
+    [Activity(Label = "CustomViewDroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var customview = FindViewById<CustomView>(Resource.Id.custom_view_1);
+            customview.names = new[] { "Bob", "John", "Paul", "Wasi", "Mark" };
+        }
+    }
+}
+
